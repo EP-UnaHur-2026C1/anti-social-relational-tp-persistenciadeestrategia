27e9cd6603b8731e0f658d7b6bb3c14b165cac0c
estefaniaalmiron <estefaniaabigail.almiron@estudiantes.unahur.edu.ar>
2026-06-04 17:45:41 -0300

    feat: CRUD comment - validaciones

---
+++ controllers/commentController.js
@@ -0,0 +1,75 @@
+const { Comment, User, Post } = require("../models");
+const { Op } = require('sequelize');
+
+const obtenerComentarios = async (req, res) => {
+  try {
+    const comentarios = await Comment.findAll({
+      where: {
+        visible: true,
+        createdAt: { [Op.gte]: req.cutoffDate }
+      },
+      attributes: ["id", "content", "userId", "postId"],
+      include: [
+        { model: User, as: "user", attributes: ['nickName', 'email'] },
+        { model: Post, as: "post", attributes: ['id', 'descripcion'] },
+      ],
+    });
+    res.status(200).json(comentarios);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener comentarios." });
+  }
+};
+
+
+const crearComentario = async (req, res) => {
+  try {
+    const { content, userId, postId } = req.body;
+
+    const comentario = await Comment.create({
+      content,
+      userId,
+      postId
+    });
+    res.status(201).json(comentario);
+    } catch (error) {
+        res.status(500).json({
+            error: "Error al crear comentario",
+    });
+  }
+};
+
+const actualizarComentario = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { content } = req.body;
+    const comentario = req.comentario;
+    await comentario.update({ content });
+    res.status(200).json(comentario);
+    } catch (error) {
+        res.status(500).json({
+          error: "Error al actualizar el comentario",
+    });
+  }
+};
+
+const eliminarComentario = async (req, res) => {
+  try {
+    const comentario = req.comentario;
+    await comentario.destroy();
+    res.status(200).json({
+      message: "Comentario eliminado correctamente",
+    });
+    } catch (error) {
+        res.status(500).json({
+            error: "Error al eliminar el comentario",
+      });
+    }
+};
+
+
+module.exports = {
+  obtenerComentarios,
+  crearComentario,
+  actualizarComentario,
+  eliminarComentario
+};
